dbdcd20cf83ccdc40627b00d902f40791c584568
flo <flo>
2019-10-20 17:04:10 +0200

    Connection to server

--- gayraudlentz-client/src/Views/Chat.tsx
+++ gayraudlentz-client/src/Views/Chat.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
+const
+    io = require("socket.io-client"),
+    ioClient = io.connect("http://localhost:8000");
+
+ioClient.on("seq-num", (msg: string) => console.info(msg));
+
 interface IProps {
 
 }
 
 function Chat(props: IProps) {
-    return <div>Admin Page</div>;
+    return <div>test</div>;
 }
 
 export default Chat;
